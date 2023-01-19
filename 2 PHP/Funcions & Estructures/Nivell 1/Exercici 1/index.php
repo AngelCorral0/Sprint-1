4a2579b3cf4ef9b02bac0f9b7b519f794405b43b
AngelCorral0 <angel.corral.olivares@gmail.com>
2023-01-19 11:53:45 +0100

    Function Changes

--- 2 PHP/Funcions & Estructures/Nivell 1/Exercici 1/index.php
+++ 2 PHP/Funcions & Estructures/Nivell 1/Exercici 1/index.php
@@ -1,12 +1,18 @@
     <?php
     // Programa una funció que, donat un número qualsevol (per exemple, la teva edat) ens digui si és parell o imparell mitjançant un missatge per pantalla.
-    $numero = 24;
+    
+   
+   echo parImpar();
+   
+    function parImpar(){
+      $numero = 24;
 
-    if ($numero %2 == 0) {
-        echo "El numero és parell";
-      }else{
-        echo "El numero és imparell";
-      }
+      if ($numero %2 == 0) {
+          echo "El numero és parell";
+        }else{
+          echo "El numero és imparell";
+        }
+    }
 
     ?>
     
--- 2 PHP/Funcions & Estructures/Nivell 1/Exercici 3/index.php
+++ 2 PHP/Funcions & Estructures/Nivell 1/Exercici 3/index.php
@@ -1,10 +1,15 @@
 <?php
 // Imagina't que volem que compti fins a un nombre diferent de 10. Programa la funció perquè el final del compte estigui parametritzat.
 
-$numdif = rand(0,100);
+echo compte();
 
-for($num = 0; $num <= $numdif; $num ++){
-    echo $num . ", ";
+function compte(){
+    $numdif = rand(0,100);
+
+    for($num = 0; $num <= $numdif; $num ++){
+        echo $num . ", ";
+    }
 }
+
 
 ?>
