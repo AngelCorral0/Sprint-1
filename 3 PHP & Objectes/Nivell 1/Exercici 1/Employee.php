19e86b4e93c795f1c917d22ac966e35475e87263
AngelCorral0 <angel.corral.olivares@gmail.com>
2023-02-15 13:11:53 +0100

    .

--- 3 PHP & Objectes/Nivell 1/Exercici 1/Employee.php
+++ 3 PHP & Objectes/Nivell 1/Exercici 1/Employee.php
@@ -1,33 +1,32 @@
 <?php
 
-class Employee{
+class Employee
+{
     public $nom;
     public $sou;
 
-public function initialize($nombre, $sueldo){
-    $this-> nom = $nombre;
-    $this-> sou = $sueldo;
-}
-public function print(){
+    public function initialize($nombre, $sueldo)
+    {
+        $this->nom = $nombre;
+        $this->sou = $sueldo;
+    }
+    public function print()
+    {
 
-    if($this->sou >= 6000){
-        echo $this->nom . " ha cobrat més de 6.000€ ". "(". $this->sou . "€)" .  ", per tant ha de pagar impostos.";
-    }else{
-        echo $this->nom . " ha cobrat menys de 6.000€ ". "(". $this->sou . "€)" .  ", per tant no ha de pagar impostos.";
+        if ($this->sou >= 6000) {
+            echo $this->nom . " ha cobrat més de 6.000€ " . "(" . $this->sou . "€)" .  ", per tant ha de pagar impostos.";
+        } else {
+            echo $this->nom . " ha cobrat menys de 6.000€ " . "(" . $this->sou . "€)" .  ", per tant no ha de pagar impostos.";
+        }
     }
 }
-}
 $Employee1 = new Employee;
-    $Employee1 ->nom = "Angel";
-    $Employee1 ->sou = 5000;
-    $Employee1 ->print();
-    echo "<br>";
+$Employee1->nom = "Angel";
+$Employee1->sou = 5000;
+$Employee1->print();
+echo "<br>";
 
-$Employee2 =new Employee;
-    $Employee2 ->nom ="Marco";
-    $Employee2 ->sou = 7000;
-    $Employee2 ->print();
-
-
-
-?>
+$Employee2 = new Employee;
+$Employee2->nom = "Marco";
+$Employee2->sou = 7000;
+$Employee2->print();
